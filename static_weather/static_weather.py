8e6561e343446b6da53be0ba127cdfa3de8d358e
alekseyteleshko@gmail.com <alekseyteleshko@gmail.com>
2024-04-08 23:55:16 +0200

    up1

--- static_weather/static_weather.py
+++ static_weather/static_weather.py
@@ -1,24 +1,32 @@
-
+from datetime import datetime
 import csv
-
-filename = 'sitka_weather_07-2014.csv'
-with open(filename) as f:
-  reader = csv.reader(f)
-  header_row = next(reader)
-
-  highs = []
-  for row in reader:
-      
-    highs.append(row[1])
-
-print(highs)
-
 from matplotlib import pyplot as plt
 
-fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(highs, c='red')
-plt.title("Daily high temperatures, July 2014", fontsize=24)
-plt.xlabel('', fontsize=16)
-plt.ylabel("Temperature (F)", fontsize=16)
-plt.tick_params(axis='both', which='major', labelsize=16)
+filename = 'sitka_weather_07-2014.csv'
+
+
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    dates, highs = [], []
+    for row in reader:
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(current_date)
+        highs.append(int(row[1]))  
+
+
+fig, ax = plt.subplots(dpi=128, figsize=(10, 6))
+
+
+ax.plot(dates, highs, c='red')
+
+
+ax.set_title("Daily high temperatures, July 2014", fontsize=24)
+ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(axis='both', which='major', labelsize=16)
+
+
 plt.show()
